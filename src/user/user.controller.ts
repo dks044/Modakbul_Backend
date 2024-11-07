6a844e93155be13c4946ea8f6e3753e48995126d
AJN <buzz781@gmail.com>
2024-11-08 03:58:51 +0900

    feat: 로그인 API 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, HttpException, HttpStatus } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
 
-@Controller('user')
-export class UserController {}
+@Controller('auth')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('signup')
+  async signup(@Body() body: { email: string; name: string; password: string }) {
+    return this.userService.create(body.email, body.name, body.password);
+  }
+
+  @Post('login')
+  async login(@Body() body: { email: string; password: string }): Promise<User | null> {
+    const user = await this.userService.findUserByEmailAndPassword(body.email, body.password);
+
+    if (!user) {
+      throw new HttpException('Invalid account', HttpStatus.UNAUTHORIZED);
+    }
+
+    return user; // 로그인 성공 시 사용자 정보를 반환
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  //회원가입
+  async create(email: string, name: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return this.prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+  }
+
+  // 로그인용
+  async findUserByEmailAndPassword(email: string, password: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user; // 비밀번호가 일치하면 사용자 반환
+    }
+
+    return null;
+  }
+}
