61ce415b6440cc985a41b9c6f2298adce6def1d8
AJN <buzz781@gmail.com>
2024-11-09 00:01:53 +0900

    feat: 회원가입 API 엔드포인트 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,24 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  // 회원가입
+  @Post('register')
+  async register(@Body() body: { email: string; name: string; password: string }) {
+    // 회원가입 처리
+    const user = await this.userService.create(body.email, body.name, body.password);
+
+    console.log('회원가입 => ', user.email);
+    // 자동 로그인 처리
+    return await this.authService.login(body.email, body.password);
+  }
 
   //로그인
   @Post('login')
